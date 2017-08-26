e24021983caa5f6b0cf88174fc86a1060b74b11b
Per Dalsten Sorensen <pdalsten@gmail.com>
2017-08-26 08:58:50 -0400

    Modified launch scripts.

--- src/main/scripts/musiclibraryboot.bat
+++ src/main/scripts/musiclibraryboot.bat
@@ -1,7 +1,8 @@
 set JAVA=%JAVA_HOME%/bin/java
 
-set VERSION=1.2
+set VERSION=1.2.1
 set DATABASE=derby
 set LOGDIR=logs
+set PORT=8080
 
-%JAVA% -jar -Dspring.profiles.active=%DATABASE% -Dlogdir=%LOGDIR% MusicLibraryBoot-%VERSION%.jar
+%JAVA% -jar -Dspring.profiles.active=%DATABASE% -Dlogdir=%LOGDIR% -Dserver.port=%PORT% MusicLibraryBoot-%VERSION%.jar
--- src/main/scripts/musiclibraryboot.sh
+++ src/main/scripts/musiclibraryboot.sh
@@ -2,8 +2,9 @@
 
 JAVA=$JAVA_HOME/bin/java
 
-VERSION=1.2
+VERSION=1.2.1
 DATABASE=derby
 LOGDIR=logs
+PORT=8080
 
-$JAVA -jar -Dspring.profiles.active=$DATABASE -Dlogdir=$LOGDIR MusicLibraryBoot-$VERSION.jar
+$JAVA -jar -Dspring.profiles.active=$DATABASE -Dlogdir=$LOGDIR -Dserver.port=$PORT MusicLibraryBoot-$VERSION.jar
