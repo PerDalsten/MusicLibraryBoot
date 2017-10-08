82470fa35f460231ef2d76894e9ab3abab3ffc39
Per Dalsten Sorensen <pdalsten@gmail.com>
2017-10-08 17:24:38 -0400

    Moved to current

--- src/test/java/dk/purplegreen/musiclibraryboot/MusicLibraryBootApplicationTests.java
+++ src/test/java/dk/purplegreen/musiclibraryboot/MusicLibraryBootApplicationTests.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import dk.purplegreen.musiclibraryboot.repository.AlbumRepository;
@@ -16,6 +17,7 @@ import dk.purplegreen.musiclibraryboot.service.MusicLibraryService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 public class MusicLibraryBootApplicationTests {
 
 	@Autowired
