12592c17c0328116825b2298e0a7bd4125e5f644
Per Dalsten Sorensen <pdalsten@gmail.com>
2017-11-17 11:20:50 -0500

    current is default profile

--- src/main/scripts/musiclibraryboot.bat
+++ src/main/scripts/musiclibraryboot.bat
@@ -1,8 +1,10 @@
 set JAVA=%JAVA_HOME%/bin/java
 
-set VERSION=1.2.1
-set DATABASE=derby
+set VERSION=1.2.2
+set PROFILE=current
 set LOGDIR=logs
 set PORT=8080
 
-%JAVA% -jar -Dspring.profiles.active=%DATABASE% -Dlogdir=%LOGDIR% -Dserver.port=%PORT% MusicLibraryBoot-%VERSION%.jar
+if not "%1"=="" set PROFILE=%1  
+
+%JAVA% -jar -Dspring.profiles.active=%PROFILE% -Dlogdir=%LOGDIR% -Dserver.port=%PORT% MusicLibraryBoot-%VERSION%.jar
